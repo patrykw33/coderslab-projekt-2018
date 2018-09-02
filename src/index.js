a789d28908b3bee94b8edaf311501795d3479ae9
patrykw33 <patryk.watras@wp.pl>
2018-09-02 10:41:36 +0200

    fixes paths

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import styles from '../style.scss';
+import '../style.scss';
 
 ReactDOM.render(
     <App/>,
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,15 @@ module.exports = {
 
                     }
                 }
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
             }
 
         ]
